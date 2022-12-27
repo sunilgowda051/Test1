d90d288adb950f806b076393ae5bde7e1187f4b9
Sunil <Sunil@DESKTOP-J563LG5>
2022-12-27 15:34:01 +0530

    build 102

---
+++ SUNIL/src/METHOD/elipse.java
@@ -0,0 +1,18 @@
+package METHOD;
+
+public class elipse 
+	{
+	static void area()
+	{
+		int a = 5;
+		int b = 6;
+		double c =3.142*a*b;
+		System.out.println(c);
+	}
+	public static void main(String[]args)
+	{
+		System.out.println("area of ellipse is");
+		area();
+	}
+	}
+
