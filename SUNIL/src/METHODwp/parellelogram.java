47c6179f1017dc69fc5169034bf25ecf79d5ab81
Sunil <Sunil@DESKTOP-J563LG5>
2022-12-27 15:37:33 +0530

    build 101

---
+++ SUNIL/src/METHODwp/parellelogram.java
@@ -0,0 +1,16 @@
+package METHODwp;
+
+public class parellelogram 
+{
+	static void area(int b,int h)
+	{
+	int a = b*h;
+
+		System.out.println(a);
+	}
+	public static void main(String[]args)
+	{
+		System.out.println("area of parellelogram is");
+		area(8,5);
+	}
+}
